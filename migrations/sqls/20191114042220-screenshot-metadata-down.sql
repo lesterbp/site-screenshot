79c7c927e64b360981a991aedda05db61cbb0bc6
Lester.Prudente <lester_b_p@yahoo.com>
2019-11-15 20:24:20 +0800

    updated migration script

--- migrations/sqls/20191114042220-screenshot-metadata-down.sql
+++ migrations/sqls/20191114042220-screenshot-metadata-down.sql
@@ -1,2 +1,3 @@
+DROP INDEX index_session_id_on_screenshot_metadata;
 DROP TABLE IF EXISTS screenshot_metadata;
 DROP FUNCTION IF EXISTS "set_updated_at"();
--- migrations/sqls/20191114042220-screenshot-metadata-up.sql
+++ migrations/sqls/20191114042220-screenshot-metadata-up.sql
@@ -9,11 +9,14 @@ $$ language 'plpgsql';
 CREATE TABLE screenshot_metadata
 (
   id TEXT NOT NULL,
-  screenshot_metadata JSONB DEFAULT '[]'::jsonb,
+  session_id TEXT NOT NULL,
+  file_path TEXT NOT NULL,
   created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
 );
+
+CREATE INDEX index_session_id_on_screenshot_metadata ON screenshot_metadata (session_id);
 
 CREATE TRIGGER update_screenshot_metadata
   BEFORE UPDATE
