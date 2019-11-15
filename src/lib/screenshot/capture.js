a4b5f5756f4a2eeac8d6b197b94b419ad1c7b377
Lester.Prudente <lester_b_p@yahoo.com>
2019-11-15 18:40:28 +0800

    completed unit test for capture

--- src/lib/screenshot/capture.js
+++ src/lib/screenshot/capture.js
@@ -1,16 +1,27 @@
 const puppeteer = require('puppeteer-core')
+const { getLogger } = require('../logging/logger')
 
 exports.takeScreenshot = async (url, folder, filename = 'capture', ) => {
+  const log = getLogger()
+
+  const executablePath = process.env.CHROMIUM_PATH
+  log.debug('takeScreenshot: launching browser', { executablePath })
+
   const browser = await puppeteer.launch({
-    executablePath: process.env.CHROMIUM_PATH,
+    executablePath,
     args: ['--no-sandbox', '--disable-dev-shm-usage'],
   })
   const page = await browser.newPage()
+
+  log.debug('takeScreenshot: loading page', { url })
   await page.goto(url, { waitUntil: 'networkidle0', timeout: 15000 })
 
   const folderPath = folder ? `/${folder}` : ''
   const path = `${process.env.SCREENSHOTS_PATH}${folderPath}/${filename}.png`
+
+  log.debug('takeScreenshot: capturing screenshot', { path })
   await page.screenshot({path, fullPage: true})
+  log.info('takeScreenshot: screenshot taken', { url, path })
 
   await browser.close()
 }
--- test/unit/lib/screenshot/capture.spec.js
+++ test/unit/lib/screenshot/capture.spec.js
@@ -1,16 +1,50 @@
 const { describe, it } = require('mocha')
 const { expect } = require('chai')
+const sinon = require('sinon')
+const puppeteer = require('puppeteer-core')
 const { getLogger } = require('../../../../src/lib/logging/logger')
 const { takeScreenshot } = require('../../../../src/lib/screenshot/capture')
+
+const sandbox = sinon.createSandbox()
 
 getLogger({ level: 'error' }) // only log errors
 
 describe('capture', () => {
+  let launchStub
+  let gotoStub
+  let screenshotStub
+
+  beforeEach(() => {
+    gotoStub = sandbox.stub().resolves()
+    screenshotStub = sandbox.stub().resolves()
+
+    launchStub = sandbox.stub(puppeteer, 'launch').resolves({
+      newPage: async () => ({
+        goto: gotoStub,
+        screenshot: screenshotStub,
+      }),
+      close: () => {},
+    })
+  })
+
+  afterEach(() => {
+    sandbox.restore()
+  })
+
   describe('takeScreenshot', () => {
-    const urlInput = 'http://www.google.com'
-    it('works', async () => {
-      await takeScreenshot(urlInput, '', 'it_works')
-      // expect() // TODO: mock of puppeteer should be called and browser is closed
+    const urlInput = 'http://www.example.com'
+
+    it('opens page and takes screenshot', async () => {
+      await takeScreenshot(urlInput, 'test_folder', 'test_file')
+
+      expect(launchStub.calledOnce).to.be.equal(true)
+      expect(gotoStub.calledOnce).to.be.equal(true)
+      expect(screenshotStub.calledOnce).to.be.equal(true)
+      expect(gotoStub.getCall(0).args[0]).to.be.equal(urlInput)
+
+      const screenshotArg = screenshotStub.getCall(0).args[0]
+      expect(screenshotArg).to.have.property('path')
+      expect(screenshotArg.path).to.have.string('/test_folder/test_file.png')
     })
   })
 })
